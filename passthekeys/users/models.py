da297102be8801179b02a54148adb1770d1b0b59
Edward Ward <edwardwardward@gmail.com>
2017-07-17 12:29:55 +0100

    add primary_city

--- passthekeys/users/models.py
+++ passthekeys/users/models.py
@@ -9,10 +9,22 @@ from django.utils.translation import ugettext_lazy as _
 @python_2_unicode_compatible
 class User(AbstractUser):
 
+    CITY_CHOICES = (
+    ('BA','Bath'),
+    ('BN','Brighton'),
+    ('BS','Bristol'),
+    ('CB','Cambridge'),
+    ('EH','Edinburgh'),
+    ('L', 'London'),
+    ('M', 'Manchester'),
+    ('OX', 'Oxford'),
+    )
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
     phone_number = PhoneNumberField(blank=True)
+    primary_city = models.CharField(max_length=20,choices=CITY_CHOICES,blank=True)
+
 
     def __str__(self):
         return self.username
--- passthekeys/users/views.py
+++ passthekeys/users/views.py
@@ -23,7 +23,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
 
-    fields = ['name', 'phone_number']
+    fields = ['name', 'phone_number', 'primary_city']
 
     # we already imported User in the view code above, remember?
     model = User
