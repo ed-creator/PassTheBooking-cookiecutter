72f1906edd89ce41d9005ec68f075b9ab0080710
Edward Ward <edwardwardward@gmail.com>
2017-07-23 21:20:03 +0100

    error fix

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/edwardward/Projects/PassTheBooking-cookiecutter/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
